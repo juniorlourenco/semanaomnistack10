a9d771038682e039a7e45530bd0f153575e5715b
juniorlourenco <56567317+juniorlourenco@users.noreply.github.com>
2020-02-10 00:05:17 -0400

    commit

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -36,8 +36,6 @@ module.exports = {
         })
         }
 
-        
-
         return response.json(dev);
         }
     };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const mongoose = require ('mongoose')
+const cors = require ('cors');
 const routes = require ('./routes');
 
 const app = express ();
@@ -10,6 +11,7 @@ mongoose.connect('mongodb+srv://juniorlourenco:fuckuguy8@cluster0-nqouv.mongodb.
 });
 console.log ('Connected to database.')
 
+app.use (cors ());
 app.use (express.json());
 app.use (routes);
 // metodos HTTP: get, post, put, delete
